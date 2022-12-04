04ff0531036ac5e689c58fa35367b083c242bc6e
Mohamud Haroon <52501278+MoHaroon777@users.noreply.github.com>
2022-12-04 16:10:14 +0000

    fixed merge issue

--- store/admin.py
+++ store/admin.py
@@ -10,7 +10,6 @@ class GenreAdmin(admin.ModelAdmin):
 admin.site.register(Genre, GenreAdmin)
 
 class EbookAdmin(admin.ModelAdmin):
-<<<<<<< HEAD
     # modify display fields to a certain size.
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={'size':'20'})},
@@ -18,9 +17,6 @@ class EbookAdmin(admin.ModelAdmin):
     }
     
     list_display = ['name', 'price', 'description', 'available', 'date_added', 'updated']
-=======
-    list_display = ['name', 'price', 'available', 'date_added', 'updated']
->>>>>>> 7fb8e24f7d2557ac69a5270ff8fc3c5fcd892281
     list_editable = ['price', 'available']
     list_per_page = 15
 admin.site.register(Ebook, EbookAdmin)
