4cdeb4ed9234cf9395c9d96041d6c1ffd61765d4
Ken <keneithatillo@gmail.com>
2022-12-08 22:39:25 +0000

    added user profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from .views import ProfileEditView, ProfilePageView, SignUpView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('edit_profile/<int:pk>/', ProfileEditView.as_view(), name='edit_profile'),
+    path('profile/<int:pk>/', ProfilePageView.as_view(), name='view_profile'),
 ]
