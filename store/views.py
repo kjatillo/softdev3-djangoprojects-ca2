98616aa773d4691a80b4dfde36c087b88155cf8a
Ken <keneithatillo@gmail.com>
2022-12-10 20:14:38 +0000

    added manager user and modifications accross multiple apps

--- store/views.py
+++ store/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Genre, Ebook
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 # Create your views here.
+
+
+class BookCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = 'store.add_ebook'
+    model = Ebook
+    template_name = 'ebook_new.html'
+    fields = (
+        'name',
+        'author',
+        'price',
+        'isbn',
+        'image',
+        'genre',
+        'description',
+    )
+
 
 def book_view(request, genre_id=None):
     genre = None
@@ -12,6 +30,7 @@ def book_view(request, genre_id=None):
         'genre': genre,
         'ebooks': books
     })
+
 
 def book_detail(request, genre_id, book_id):
     book = get_object_or_404(Ebook, genre_id=genre_id, id=book_id)
