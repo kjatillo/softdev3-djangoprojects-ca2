f312bb9a84427bf51f40964f903c8ddf844221f7
Mohamud Haroon <52501278+MoHaroon777@users.noreply.github.com>
2022-12-10 21:43:01 +0000

    modified ebook_project/urls.py url concatination compatibility

--- ebook_project/urls.py
+++ ebook_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
