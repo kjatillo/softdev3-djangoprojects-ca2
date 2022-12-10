465a83fdaf7a3e2b99a72cbeb49f3e6f96687fe4
Mohamud Haroon <52501278+MoHaroon777@users.noreply.github.com>
2022-12-10 22:00:03 +0000

    merging branch

--- store/urls.py
+++ store/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.book_view, name='all_books'),
     path('<uuid:genre_id>/', views.book_view, name = 'books_by_genre'),
     path('<uuid:genre_id>/<uuid:book_id>', views.book_detail, name = 'book_detail'),
+    path('new/', views.BookCreateView.as_view(), name='book_create'),
 ]
